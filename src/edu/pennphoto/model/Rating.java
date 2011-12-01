3dc8993aaf5dc91cac47fe776583f9a6306b3ef5
Mark Francisco <mark.v.francisco@gmail.com>
2011-12-01 17:00:08 -0500

    Added Ratings, Tags to Photos. Added InterestIDs to User

---
+++ src/edu/pennphoto/model/Rating.java
@@ -0,0 +1,33 @@
+package edu.pennphoto.model;
+
+public class Rating {
+	private int photoID;
+	private int userID;
+	private int value;
+	public Rating(int photoID, int userID, int value) {
+		super();
+		this.photoID = photoID;
+		this.userID = userID;
+		this.value = value;
+	}
+	public int getPhotoID() {
+		return photoID;
+	}
+	public void setPhotoID(int photoID) {
+		this.photoID = photoID;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
+}
