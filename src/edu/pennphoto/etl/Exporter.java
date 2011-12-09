8778516bb5e36cd57025705433aae2dffeaf6973
Mark Francisco <mark.v.francisco@gmail.com>
2011-12-09 17:07:50 -0500

    Added date parsing

--- src/edu/pennphoto/etl/Exporter.java
+++ src/edu/pennphoto/etl/Exporter.java
@@ -1,5 +1,6 @@
 package edu.pennphoto.etl;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -9,13 +10,18 @@ import edu.pennphoto.model.User.Gender;
 public class Exporter {
 
 	public static final String TAB = "    ";
-	public static final String TAB2 = TAB + TAB;
-	public static final String TAB3 = TAB2 + TAB;
+	public static final SimpleDateFormat DOB_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+	public static final SimpleDateFormat UPLOAD_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+	
+	private static final String TAB2 = TAB + TAB;
+	private static final String TAB3 = TAB2 + TAB;
+	private static final String TAB4 = TAB3 + TAB;
 	private static final String START = "<tns:";
 	private static final String END = "</tns:";
 	private static final String PROF = "professor>\n";
 	private static final String STUD = "student>\n";
 	private static final String ID = "id>";
+	private static final String EMAIL = "email>";
 	private static final String FNAME = "first_name>";
 	private static final String LNAME = "last_name>";
 	private static final String DOB = "dob>";
@@ -38,11 +44,20 @@ public class Exporter {
 	private static final String MAJOR = "major>";
 	private static final String GPA = "gpa>";
 	private static final String ADVISOR_ID = "advisor_id>";
+	private static final String PHOTO = "photo>\n";
+	private static final String URL = "url>";
+	private static final String PRIVATE = "is_private>";
+	private static final String UPLOAD_DATE = "upload_date>";
+	private static final String RATING = "rating>\n";
+	private static final String USER_ID = "user_id>";
+	private static final String VALUE = "value>";
+	private static final String TAG = "tag>\n";
+	private static final String TAG_TEXT = "tag_text>";
 	
 	private static final String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
 						"<tns:photodb xmlns:tns=\"pennphoto17ns\"\n" +
 						TAB + "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
-						TAB + "xsi:schemaLocation=\"pennphoto17ns pennphoto17.xsd \">\n";	
+						TAB + "xsi:schemaLocation=\"pennphoto17ns pennphoto17.xsd\">\n";	
 	private static final String FOOTER = "</tns:photodb>\n";
 	
 	public static void main(String[] args){
@@ -56,20 +71,24 @@ public class Exporter {
 		xmlBuilder.append(HEADER);
 		for(User user : users){
 			boolean isProfessor = user instanceof Professor;
-			xmlBuilder.append(START + (isProfessor ? PROF : STUD));
-			xmlBuilder.append(TAB + START + ID + user.getUserID() + END + ID + "\n");
-			xmlBuilder.append(TAB + START + FNAME + user.getFirstName() + END + FNAME + "\n");
-			xmlBuilder.append(TAB + START + LNAME + user.getLastName() + END + LNAME + "\n");
-			xmlBuilder.append(TAB + START + DOB + user.getDob() + END + DOB + "\n");
-			xmlBuilder.append(TAB + START + STADD + user.getAddress() + END + STADD + "\n");
-			xmlBuilder.append(TAB + START + CITY + user.getCity() + END + CITY + "\n");
-			xmlBuilder.append(TAB + START + STATE + user.getState() + END + STATE + "\n");
-			xmlBuilder.append(TAB + START + ZIP + user.getZip() + END + ZIP + "\n");
-			xmlBuilder.append(TAB + START + ISPROF + isProfessor + END + ISPROF + "\n");
-			xmlBuilder.append(TAB + START + GENDER + user.getGender() + END + GENDER + "\n");
+			xmlBuilder.append(TAB + START + (isProfessor ? PROF : STUD));
+			xmlBuilder.append(TAB2 + START + ID + user.getUserID() + END + ID + "\n");
+			xmlBuilder.append(TAB2 + START + EMAIL + user.getEmail() + END + EMAIL + "\n");
+			xmlBuilder.append(TAB2 + START + FNAME + user.getFirstName() + END + FNAME + "\n");
+			xmlBuilder.append(TAB2 + START + LNAME + user.getLastName() + END + LNAME + "\n");
+			xmlBuilder.append(TAB2 + START + DOB + DOB_FORMAT.format(user.getDob()) + END + DOB + "\n");
+			xmlBuilder.append(TAB2 + START + STADD + user.getAddress() + END + STADD + "\n");
+			xmlBuilder.append(TAB2 + START + CITY + user.getCity() + END + CITY + "\n");
+			xmlBuilder.append(TAB2 + START + STATE + user.getState() + END + STATE + "\n");
+			xmlBuilder.append(TAB2 + START + ZIP + user.getZip() + END + ZIP + "\n");
+			xmlBuilder.append(TAB2 + START + ISPROF + isProfessor + END + ISPROF + "\n");
+			xmlBuilder.append(TAB2 + START + GENDER + user.getGender() + END + GENDER + "\n");
 			appendInterests(xmlBuilder, user);
 			appendAttendances(xmlBuilder, user);
-			appendPhotos(xmlBuilder, user);
+			
+			//TODO retrieve photos by ownerId			
+			//appendPhotos(xmlBuilder, photos);
+			
 			appendCircles(xmlBuilder, user);
 			if(isProfessor){
 				appendProfAttributes(xmlBuilder, (Professor) user);
@@ -77,7 +96,7 @@ public class Exporter {
 				appendStudAttributes(xmlBuilder, (Student) user);
 			}
 
-			xmlBuilder.append(END + (isProfessor ? PROF : STUD));
+			xmlBuilder.append(TAB + END + (isProfessor ? PROF : STUD));
 		}
 		xmlBuilder.append(FOOTER);
 		return xmlBuilder.toString();
@@ -100,9 +119,6 @@ public class Exporter {
 		}
 	}
 	
-	private static void appendPhotos(StringBuilder xmlBuilder, User user){
-		//TODO implement appendPhotos()
-	}
 	
 	private static void appendCircles(StringBuilder xmlBuilder, User user){
 		for(Circle circle : user.getCircles()){
@@ -127,6 +143,31 @@ public class Exporter {
 		xmlBuilder.append(TAB2 + START + TITLE + professor.getTitle() + END + TITLE + "\n");
 		for(Integer id : professor.getAdviseeIds()){
 			xmlBuilder.append(TAB2 + START + ADVISEE_ID + id + END + ADVISEE_ID + "\n");
+		}
+	}
+	
+	private static void appendPhotos(StringBuilder xmlBuilder, List<Photo> photos){
+		for(Photo photo : photos){
+			boolean isPrivate = photo.isPrivate();
+			xmlBuilder.append(TAB2 + START + PHOTO);
+			xmlBuilder.append(TAB3 + START + ID + photo.getPhotoId() + END + ID + "\n");
+			xmlBuilder.append(TAB3 + START + URL + photo.getUrl() + END + URL + "\n");
+			xmlBuilder.append(TAB3 + START + PRIVATE + isPrivate + END + PRIVATE + "\n");
+			xmlBuilder.append(TAB3 + START + UPLOAD_DATE + 
+					UPLOAD_DATE_FORMAT.format(photo.getUploadDate()) + END + ID + "\n");
+			for(Rating rating : photo.getRatings()){
+				xmlBuilder.append(TAB3 + START + RATING);
+				xmlBuilder.append(TAB4 + START + USER_ID + rating.getUserID() + END + USER_ID + "\n");
+				xmlBuilder.append(TAB4 + START + VALUE + rating.getValue() + END + VALUE + "\n");
+				xmlBuilder.append(TAB3 + END + RATING);
+			}
+			for(Tag tag : photo.getTags()){
+				xmlBuilder.append(TAB3 + START + TAG);
+				xmlBuilder.append(TAB4 + START + USER_ID + tag.getUserID() + END + USER_ID + "\n");
+				xmlBuilder.append(TAB4 + START + VALUE + tag.getTagText() + END + VALUE + "\n");
+				xmlBuilder.append(TAB3 + END + TAG);
+			}
+			xmlBuilder.append(TAB2 + END + PHOTO);
 		}
 	}
 
--- src/edu/pennphoto/test/ExporterTest.java
+++ src/edu/pennphoto/test/ExporterTest.java
@@ -2,6 +2,7 @@ package edu.pennphoto.test;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
 import java.util.Date;
 
 import edu.pennphoto.model.*;
@@ -13,14 +14,17 @@ import org.junit.Test;
 
 public class ExporterTest {
 	
-	User user1;
+	Professor prof1;
 	StringBuilder sb;
 	public static final String TAB = Exporter.TAB;
 
 	@Before
-	public void setup(){
-		user1 = new Professor(1, "user1@penn.edu", "First 1", "Last 1", new Date(), 
+	public void setup() throws ParseException{
+		Date dob = Exporter.DOB_FORMAT.parse("1986-07-19");
+		prof1 = new Professor(1, "user1@penn.edu", "First 1", "Last 1", dob, 
 				Gender.MALE, "Primero Street", "Isa City", "NY", "10001");
+		prof1.setTitle("Director");
+		prof1.setResearchArea("database");
 		sb = new StringBuilder();
 		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
 		sb.append("<tns:photodb xmlns:tns=\"pennphoto17ns\"\n");
@@ -28,21 +32,23 @@ public class ExporterTest {
 		sb.append(TAB + "xsi:schemaLocation=\"pennphoto17ns pennphoto17.xsd\">\n");
 		sb.append(TAB + "<tns:professor>\n");
 		sb.append(TAB + TAB + "<tns:id>1</tns:id>\n");
-		sb.append(TAB + TAB + "<tns:email>user1@upenn.edu</tns:email>\n");
+		sb.append(TAB + TAB + "<tns:email>user1@penn.edu</tns:email>\n");
 		sb.append(TAB + TAB + "<tns:first_name>First 1</tns:first_name>\n");
 		sb.append(TAB + TAB + "<tns:last_name>Last 1</tns:last_name>\n");
 		sb.append(TAB + TAB + "<tns:dob>1986-07-19</tns:dob>\n");
 		sb.append(TAB + TAB + "<tns:street_address>Primero Street</tns:street_address>\n");
 		sb.append(TAB + TAB + "<tns:city>Isa City</tns:city>\n");
 		sb.append(TAB + TAB + "<tns:state>NY</tns:state>\n");
-		sb.append(TAB + TAB + "<tns:zip_code>10001</tns:zipcode>\n");
+		sb.append(TAB + TAB + "<tns:zip_code>10001</tns:zip_code>\n");
 		sb.append(TAB + TAB + "<tns:is_professor>true</tns:is_professor>\n");
 		sb.append(TAB + TAB + "<tns:gender>m</tns:gender>\n");
+		sb.append(TAB + TAB + "<tns:research_area>database</tns:research_area>\n");
+		sb.append(TAB + TAB + "<tns:title>Director</tns:title>\n");
 	}
 
 	@Test
 	public void testSingleUserExport() {
-		String actual = Exporter.export(user1);
+		String actual = Exporter.export(prof1);
 		sb.append(TAB + "</tns:professor>\n");
 		sb.append("</tns:photodb>\n");
 		String expected = sb.toString();
