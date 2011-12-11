73b050862d07924b556b1d81e8288d96266a724f
Narine <narine@seas.upenn.edu>
2011-12-10 23:13:36 -0500

    relevance score, sort by rel_score in search by tag

--- src/edu/pennphoto/db/PhotoDAO.java
+++ src/edu/pennphoto/db/PhotoDAO.java
@@ -14,6 +14,14 @@ import edu.pennphoto.model.Tag;
 
 public class PhotoDAO {
 
+	private static String RELEVANCE_SCORE_QUERY = "select pp.id as photo_id, IF(avg(rating) IS NOT NULL, avg(rating), 0 ) "+
+			"+ IF((select 1 from Friendship_View where user_id=? and friend_id=pp.owner_id), 1, 0) +IF(pp.owner_id=?, 1, 0) "+
+			"+ IF(favg.favg is null, 0, favg.favg) as rel_score from Photo pp left join Rating r on pp.id=r.photo_id "+ 
+			"left join (select photo_id, avg(rating) favg from Rating where user_id IN (select friend_id from Friendship_View where user_id=?) group by photo_id) favg on pp.id = favg.photo_id " +
+			"where pp.id IN ("+
+			"select distinct p.id from Photo p left join Photo_Visible_To_Circle vcr on p.id=vcr.photo_id "+ 
+			"left join Photo_Visible_To_User vu on p.id=vu.photo_id left join In_Circle ic on vcr.circle_id = ic.circle_id where is_private = 0 or vu.user_id = ? or ic.friend_id=? or p.owner_id=?) group by pp.id"; 
+	
 	public static boolean postPhoto(Photo photo) throws SQLException {
 		Connection conn = null;
 		PreparedStatement stmt = null;
@@ -156,20 +164,29 @@ public class PhotoDAO {
 	}
 
 	public static List<Photo> searchPhotosByTag(String keyword, int userId) {
-
-		String query = "select * from Photo p left join Tag t on p.id=t.photo_id where t.tag=? and"
-				+ " p.id IN (select distinct p.id from Photo p left join Photo_Visible_To_Circle vcr on p.id=vcr.photo_id left join Photo_Visible_To_User vu on p.id=vu.photo_id"
-				+ " left join In_Circle ic on vcr.circle_id = ic.circle_id where is_private = 0 or vu.user_id = ? or ic.friend_id=? or p.owner_id=?)";
+		String query = "select * from Photo p left join Tag t on p.id=t.photo_id inner join ("+RELEVANCE_SCORE_QUERY+") "+
+	" rel on p.id=rel.photo_id where t.tag=? order by rel.rel_score";
+//		String query = "select * from Photo p left join Tag t on p.id=t.photo_id where t.tag=? and"
+//				+ " p.id IN (select distinct p.id from Photo p left join Photo_Visible_To_Circle vcr on p.id=vcr.photo_id left join Photo_Visible_To_User vu on p.id=vu.photo_id"
+//				+ " left join In_Circle ic on vcr.circle_id = ic.circle_id where is_private = 0 or vu.user_id = ? or ic.friend_id=? or p.owner_id=?)";
 
 		PreparedStatement stmt = null;
 		Connection conn = null;
 		try {
 			conn = DBHelper.getInstance().getConnection();
 			stmt = conn.prepareStatement(query);
-			stmt.setString(1, keyword);
+//			stmt.setString(1, keyword);
+//			stmt.setInt(2, userId);
+//			stmt.setInt(3, userId);
+//			stmt.setInt(4, userId);
+			
+			stmt.setInt(1, userId);
 			stmt.setInt(2, userId);
 			stmt.setInt(3, userId);
 			stmt.setInt(4, userId);
+			stmt.setInt(5, userId);
+			stmt.setInt(6, userId);
+			stmt.setString(7, keyword);
 			ResultSet rs = stmt.executeQuery();
 			return generatePhotoList(rs);
 		} catch (Exception e) {
