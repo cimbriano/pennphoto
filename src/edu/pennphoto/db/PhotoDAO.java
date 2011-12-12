e9c1b6586ca37fd8a74072a4b03f4fb3a6fa67c5
Narine <narine@seas.upenn.edu>
2011-12-11 22:29:26 -0500

    get top photos, get recent events fro user

--- src/edu/pennphoto/db/PhotoDAO.java
+++ src/edu/pennphoto/db/PhotoDAO.java
@@ -8,19 +8,37 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.pennphoto.model.Event;
+import edu.pennphoto.model.Event.EventType;
 import edu.pennphoto.model.Photo;
 import edu.pennphoto.model.Rating;
 import edu.pennphoto.model.Tag;
 
 public class PhotoDAO {
+	
+	private static final String PHOTO_VISIBILITY_QUERY = "select distinct p.id from Photo p left join Photo_Visible_To_Circle vcr on p.id=vcr.photo_id "+ 
+			"left join Photo_Visible_To_User vu on p.id=vu.photo_id left join In_Circle ic on vcr.circle_id = ic.circle_id where is_private = 0 or vu.user_id = ? or ic.friend_id=? or p.owner_id=?";
 
-	private static String RELEVANCE_SCORE_QUERY = "select pp.id as photo_id, IF(avg(rating) IS NOT NULL, avg(rating), 0 ) "+
+	private static final String RELEVANCE_SCORE_QUERY = "select pp.id as photo_id, IF(avg(rating) IS NOT NULL, avg(rating), 0 ) "+
 			"+ IF((select 1 from Friendship_View where user_id=? and friend_id=pp.owner_id), 1, 0) +IF(pp.owner_id=?, 1, 0) "+
 			"+ IF(favg.favg is null, 0, favg.favg) as rel_score from Photo pp left join Rating r on pp.id=r.photo_id "+ 
 			"left join (select photo_id, avg(rating) favg from Rating where user_id IN (select friend_id from Friendship_View where user_id=?) group by photo_id) favg on pp.id = favg.photo_id " +
-			"where pp.id IN ("+
-			"select distinct p.id from Photo p left join Photo_Visible_To_Circle vcr on p.id=vcr.photo_id "+ 
-			"left join Photo_Visible_To_User vu on p.id=vu.photo_id left join In_Circle ic on vcr.circle_id = ic.circle_id where is_private = 0 or vu.user_id = ? or ic.friend_id=? or p.owner_id=?) group by pp.id"; 
+			"where pp.id IN ("+ PHOTO_VISIBILITY_QUERY +") group by pp.id"; 
+	
+	
+	/**
+	 * Returns photos uploaded by friends which are visible to specified user
+	 */
+	private static final String PHOTO_EVENTS_QUERY = "select id as photo_id, owner_id as user_id, 'PHOTO' as event_type, url as event_value, upload_date as event_date from Photo pp where pp.id IN ( "+ PHOTO_VISIBILITY_QUERY +" )"+
+"and EXISTS (select 1 from Friendship_View where user_id=? and friend_id=pp.owner_id)";
+	
+	/**
+	 * Returns tags done by friends on the photos visible to specified user
+	 */
+	private static final String TAG_EVENTS_QUERY = "select photo_id, user_id, 'TAG' as event_type, tag as event_value, tag_date as event_date from Photo pp left join Tag t on pp.id=t.photo_id where t.photo_id IN ( "+ PHOTO_VISIBILITY_QUERY +" )"+
+			"and EXISTS (select 1 from Friendship_View where user_id=? and friend_id=t.user_id)";
+	
+	private static final String RECENT_UPDATES_QUERY = "select * from ("+PHOTO_EVENTS_QUERY+" union "+TAG_EVENTS_QUERY+") as fullResult order by event_date desc limit 20";
 	
 	public static boolean postPhoto(Photo photo) throws SQLException {
 		Connection conn = null;
@@ -163,23 +181,49 @@ public class PhotoDAO {
 		}
 	}
 
+	/**
+	 * Returns 20 top scoring photos for the given user in accordance with relevance score
+	 * @param userId
+	 * @return list of top photos
+	 */
+	public static List<Photo> getTopPhotosForUser(int userId) {
+		String query = "select * from Photo p inner join ("+RELEVANCE_SCORE_QUERY+") "+
+	" rel on p.id=rel.photo_id order by rel.rel_score desc limit 20";
+
+		PreparedStatement stmt = null;
+		Connection conn = null;
+		try {
+			conn = DBHelper.getInstance().getConnection();
+			stmt = conn.prepareStatement(query);		
+			stmt.setInt(1, userId);
+			stmt.setInt(2, userId);
+			stmt.setInt(3, userId);
+			stmt.setInt(4, userId);
+			stmt.setInt(5, userId);
+			stmt.setInt(6, userId);
+			ResultSet rs = stmt.executeQuery();
+			return generatePhotoList(rs);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Returns list of photos visible to specified person with specified tag ordered by relevance score
+	 * @param keyword
+	 * @param userId
+	 * @return
+	 */
 	public static List<Photo> searchPhotosByTag(String keyword, int userId) {
 		String query = "select * from Photo p left join Tag t on p.id=t.photo_id inner join ("+RELEVANCE_SCORE_QUERY+") "+
-	" rel on p.id=rel.photo_id where t.tag=? order by rel.rel_score";
-//		String query = "select * from Photo p left join Tag t on p.id=t.photo_id where t.tag=? and"
-//				+ " p.id IN (select distinct p.id from Photo p left join Photo_Visible_To_Circle vcr on p.id=vcr.photo_id left join Photo_Visible_To_User vu on p.id=vu.photo_id"
-//				+ " left join In_Circle ic on vcr.circle_id = ic.circle_id where is_private = 0 or vu.user_id = ? or ic.friend_id=? or p.owner_id=?)";
+	" rel on p.id=rel.photo_id where t.tag=? order by rel.rel_score desc";
 
 		PreparedStatement stmt = null;
 		Connection conn = null;
 		try {
 			conn = DBHelper.getInstance().getConnection();
 			stmt = conn.prepareStatement(query);
-//			stmt.setString(1, keyword);
-//			stmt.setInt(2, userId);
-//			stmt.setInt(3, userId);
-//			stmt.setInt(4, userId);
-			
 			stmt.setInt(1, userId);
 			stmt.setInt(2, userId);
 			stmt.setInt(3, userId);
@@ -195,6 +239,40 @@ public class PhotoDAO {
 		}
 	}
 
+	/**
+	 * Returns latests events for specified user
+	 * It includes posting photos by friends of the user (if photo is visible to him)
+	 * or tagging by friends of the user on the photos visible to him
+	 * 
+	 * @param userId
+	 * @return
+	 */
+	public static List<Event> getRecentEvents(int userId) {
+		PreparedStatement stmt = null;
+		Connection conn = null;
+		try {
+			conn = DBHelper.getInstance().getConnection();
+			stmt = conn.prepareStatement(RECENT_UPDATES_QUERY);
+			int paramsNum = 8;
+			for(int i = 1; i <= paramsNum; i++){
+				stmt.setInt(i, userId);
+			}
+						
+			ResultSet rs = stmt.executeQuery();
+			List<Event> events = new ArrayList<Event>();
+			Event event = null;
+			while (rs.next()) {
+				//photo_id, user_id,event_type, event_value
+				event = new Event(rs.getInt("photo_id"), rs.getInt("user_id"),
+						rs.getString("event_value"), EventType.valueOf(rs.getString("event_type")));
+				events.add(event);
+			}
+			return events;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	public static List<Photo> getUserPhoto(int userId) {
 		String query = "select * from Photo where owner_id=" + userId;
 		Statement stmt = null;
@@ -302,9 +380,25 @@ public class PhotoDAO {
 	}
 
 	public static void testSearchPhoto() {
-		List<Photo> photos = searchPhotosByTag("tag from 17009", 17002);
+		List<Photo> photos = searchPhotosByTag("tag from 17009", 17001);
+		System.out.println("Search Result");
 		for (Photo photo : photos) {
 			System.out.println(photo.getPhotoId() + " " + photo.getUrl());
+		}
+		
+		System.out.println("Top photos Result");
+		photos = getTopPhotosForUser(17002);
+		for (Photo photo : photos) {
+			System.out.println(photo.getPhotoId() + " " + photo.getUrl());
+		}
+		
+		List<Event> events = getRecentEvents(17001);
+		for (Event event : events) {
+			System.out.println(event.getPhotoId());
+			System.out.println(event.getUserId());
+			System.out.println(event.getEventValue());
+			System.out.println(event.getType());
+			System.out.println("------------------");
 		}
 	}
 
